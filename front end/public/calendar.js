96baa7ab7b9221b93d7891234e19a2c6aa0afb3c
Adam <adam.matolak@kosickaakademia.sk>
2021-05-12 23:36:35 +0200

    Arrows functions init

--- front end/public/calendar.js
+++ front end/public/calendar.js
@@ -1,13 +1,35 @@
-const monthNames = ["January", "February", "March", "April", "May", "June",
+var monthChange = 0;
+
+var leftArrow = document.getElementById("leftArrow").onclick = function monthDecrease(){
+    monthChange-- ;
+    return monthChange;
+    location.reload;
+}
+
+var rightArrow = document.getElementById("rightArrow").onclick = function monthIncrease(){
+    monthChange++ ;
+    return monthChange;
+    location.reload;
+}
+
+
+
+
+
+function getCurrentDate(){
+    const monthNames = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"];
     
-const d = new Date();
+    var d = new Date();
+    var day = d.getDate();
+    var month = d.getMonth() + monthChange;
+    var year = d.getFullYear();
 
-document.getElementById("day").innerHTML = d.getDay();
-document.getElementById("month").innerHTML = monthNames[d.getMonth()];
-document.getElementById("year").innerHTML = d.getFullYear();
+    document.getElementById("day").innerHTML = day;
+    document.getElementById("month").innerHTML = monthNames[month];
+    document.getElementById("year").innerHTML = year;
+}
 
-document.getElementById("dayDate").onclick.style.backgroundColor = "blue";
-
-
-
+window.onload = leftArrow;
+window.onload = rightArrow;
+window.onload = getCurrentDate;
