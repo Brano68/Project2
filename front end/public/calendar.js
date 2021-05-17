0f2570995daeb16f50b1819d544c0c77bc6f3b3a
Adam <adam.matolak@kosickaakademia.sk>
2021-05-17 22:29:46 +0200

    Added month increase and decrease

--- front end/public/calendar.js
+++ front end/public/calendar.js
@@ -1,31 +1,56 @@
+
+
+
+
+
+// function generateCalendar(){
+//     for(var i = 1;i < 32;i++){
+//         $( "#dayDateDiv" ).append( '<div class="daysDiv" id="dayDate' + i + '"><p class="calDays">' + i + '</p></div>' );
+//     }
+// }
 var monthChange = 0;
 
-function generateCalendar(){
-    for(var i = 1;i < 32;i++){
-        $( "#dayDateDiv" ).append( '<div class="daysDiv" id="dayDate' + i + '"><p class="calDays">' + i + '</p></div>' );
+function monthDecrease(){
+    var monthChangeStored = window.localStorage.getItem("monthChangeStored");
+    
+    if(monthChangeStored==null){
+        window.localStorage.setItem("monthChangeStored", monthChange);
     }
-}
+    var monthChangeStored = window.localStorage.getItem("monthChangeStored");
+    if(monthChangeStored>=-1){
+        monthChangeStored--;
+        window.localStorage.setItem("monthChangeStored", monthChangeStored);
+    }
+    
+    location.reload();
+};
 
-
-document.getElementById("leftArrow").addEventListener("click", function monthDecrease(){
-    monthChange-- ;
-    return monthChange;
-    location.reload;
-});
-
-document.getElementById("rightArrow").onclick = function monthIncrease(){
-    monthChange++ ;
-    return monthChange;
-    location.reload;
+function monthIncrease(){
+    var monthChangeStored = window.localStorage.getItem("monthChangeStored");
+    
+    if(monthChangeStored==null){
+        window.localStorage.setItem("monthChangeStored", monthChange);
+    }
+    var monthChangeStored = window.localStorage.getItem("monthChangeStored");
+    if(monthChangeStored<=1){
+        monthChangeStored++;
+        window.localStorage.setItem("monthChangeStored", monthChangeStored);
+    }
+    location.reload();
 };
 
 function getCurrentDate(){
     const monthNames = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"];
+
+    
+    var monthChangeStored = window.localStorage.getItem("monthChangeStored");
+    
     
     var d = new Date();
     var day = d.getDate();
-    var month = d.getMonth() + monthChange;
+    var month = Number(d.getMonth()) + Number(monthChangeStored);
+    console.log(month);
     var year = d.getFullYear();
 
     document.getElementById("day").innerHTML = day;
@@ -34,4 +59,4 @@ function getCurrentDate(){
 };
 
 window.onload = getCurrentDate;
-window.onload = generateCalendar;
+//window.onload = generateCalendar;
