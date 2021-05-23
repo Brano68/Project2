ad6f93baba4a358e5766d8784f1416ad12797252
Adam <adam.matolak@kosickaakademia.sk>
2021-05-23 23:02:42 +0200

    added getOrderDate function

--- front end/public/calendar.js
+++ front end/public/calendar.js
@@ -1,13 +1,3 @@
-
-
-
-
-
-// function generateCalendar(){
-//     for(var i = 1;i < 32;i++){
-//         $( "#dayDateDiv" ).append( '<div class="daysDiv" id="dayDate' + i + '"><p class="calDays">' + i + '</p></div>' );
-//     }
-// }
 var monthChange = 0;
 
 
@@ -40,6 +30,8 @@ function monthIncrease(){
     location.reload();
 };
 
+
+
 function getCurrentDate(){
     const monthNames = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"];
@@ -51,7 +43,7 @@ function getCurrentDate(){
     var d = new Date();
     var day = d.getDate();
     var month = Number(d.getMonth()) + Number(monthChangeStored);
-    console.log(month);
+    //console.log(month);
     var year = d.getFullYear();
 
     document.getElementById("day").innerHTML = day;
@@ -93,36 +85,76 @@ function getCurrentDate(){
         
     }
 
-    function getOrderDate(clicked_id){
-        var pickedDate1 = document.getElementById(clicked_id).innerHTML
-        var pickedMonth1 = month
-        var pickedDate1Stored = window.localStorage.getItem("pickedDate1Stored")
-        var pickedMonth1Stored = window.localStorage.getItem("pickedMonth1Stored")
-        if(pickedDate1Stored==null&&pickedMonth1Stored==null){
-            window.localStorage.setItem("pickedDate1Stored",pickedDate1)
-            pickedDate1Stored = window.localStorage.getItem("pickedDate1Stored")
-            window.localStorage.setItem("pickedMonth1Stored",pickedMonth1)
-            pickedMonth1Stored = window.localStorage.getItem("pickedMonth1Stored")
-            console.log(pickedDate1Stored + pickedMonth1Stored);
-        }else{
-            pickedDate1Stored = null
-            window.localStorage.setItem("pickedDate1Stored",pickedDate1Stored)
-            pickedMonth1Stored = null
-            window.localStorage.setItem("pickedMonth1Stored",pickedMonth1Stored)
-            console.log(pickedDate1Stored + pickedMonth1Stored);
-        }
-        
-       
+    var date1final = window.localStorage.getItem("pickedDate1Stored")
+    var date2final = window.localStorage.getItem("pickedDate2Stored")
+    var month1final = window.localStorage.getItem("pickedMonth1Stored")
+    var month2final = window.localStorage.getItem("pickedMonth2Stored")
+
+    console.log(date1final);
+    console.log(month1final);
+    console.log(date2final);
+    console.log(month2final);
+
     
-        
-    
-    
-    location.reload();
-        
-    }
 
 };
 
 
+
+function getOrderDate(clicked_id){
+    var d = new Date();
+    var flag = window.localStorage.getItem("flagStored");
+    var monthChangeStored = window.localStorage.getItem("monthChangeStored");
+    var pickedDate = document.getElementById(clicked_id).firstChild.innerHTML
+    
+    var pickedMonth = Number(d.getMonth()) + Number(monthChangeStored) + 1;
+    
+    if(flag == null){
+        
+        window.localStorage.setItem("pickedDate1Stored", pickedDate)
+        window.localStorage.setItem("pickedMonth1Stored",pickedMonth)
+        
+        flag = 1;
+        window.localStorage.setItem("flagStored", flag);
+
+        location.reload();
+    }else if(flag==1){
+        var pickedDate1Stored = window.localStorage.getItem("pickedDate1Stored")
+        var pickedMonth1Stored = window.localStorage.getItem("pickedMonth1Stored")
+        if(pickedDate == pickedDate1Stored && pickedMonth == pickedMonth1Stored){
+            pickedDate1Stored = null
+            pickedMonth1Stored = null
+            
+            window.localStorage.setItem("pickedDate1Stored", pickedDate1Stored)
+            window.localStorage.setItem("pickedMonth1Stored", pickedMonth1Stored)
+            flag = null;
+            window.localStorage.setItem("flagStored", flag)
+            location.reload();
+        }else{
+            
+            window.localStorage.setItem("pickedDate2Stored",pickedDate)
+            window.localStorage.setItem("pickedMonth2Stored", pickedMonth)
+            flag = 2;
+            window.localStorage.setItem("flagStored", flag)
+            location.reload();
+        }
+    }else if(flag == 2){
+        var pickedDate2Stored = window.localStorage.getItem("pickedDate2Stored")
+        var pickedMonth2Stored = window.localStorage.getItem("pickedMonth2Stored")
+        if(pickedDate == pickedDate2Stored && pickedMonth == pickedMonth2Stored){
+            var pickedDate2Stored = null
+            var pickedMonth2Stored = null
+            window.localStorage.setItem("pickedDate2Stored",pickedDate2Stored)
+            window.localStorage.setItem("pickedMonth2Stored", pickedMonth2Stored)
+            flag = 1
+            window.localStorage.setItem("flagStored", flag)
+            location.reload();
+        }else{
+            alert("You already picked 2 dates")
+            alert(flag)
+        }
+        
+    }
+}
 
 window.onload = getCurrentDate;
