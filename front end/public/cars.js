40184168fa1af171e650df43a1d77d8a85807a4b
Adam <adam.matolak@kosickaakademia.sk>
2021-06-05 20:04:02 +0200

    added getCars function

--- front end/public/cars.js
+++ front end/public/cars.js
@@ -1,28 +1,19 @@
 
-async function getCars(url = 'https://localhost:5001/car/models?From=25.05.2021&To=30.05.2021', data = {}){
-
-  const response = await fetch(url,{
-    method: 'GET',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-      redirect: 'follow',
-      referrerPolicy: 'no-referrer',
-      body: JSON.stringify(data)
-    
-  });
-
-  return response.json();
-  console.log(response);
-
-}
-
+function getCars(){
+  var d = new Date();
+  var date1final = parseInt(window.localStorage.getItem("pickedDate1Stored")); 
+  var date2final = parseInt(window.localStorage.getItem("pickedDate2Stored")); 
+  var month1final = parseInt(window.localStorage.getItem("pickedMonth1Stored")); 
+  var month2final = parseInt(window.localStorage.getItem("pickedMonth2Stored")); 
+  
+  $.getJSON('https://localhost:5001/car/models?From='+ date1final + '.' +month1final+ '.' + d.getFullYear() + '&To=' + date2final + '.' + month2final + '.' + d.getFullYear(), function(data) {
+  console.log(data);
+  
+  
+  
 
   
 
-
-
-window.onload="getCars()";
+});
+}
+window.onload=getCars;
