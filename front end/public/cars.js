acc2200bb54b7ef29c80ffcc89ddd74c8547ceba
Adam <adam.matolak@kosickaakademia.sk>
2021-06-14 23:24:28 +0200

    cars update

--- front end/public/cars.js
+++ front end/public/cars.js
@@ -6,7 +6,7 @@ function getCars(){
   var month1final = parseInt(window.localStorage.getItem("pickedMonth1Stored")); 
   var month2final = parseInt(window.localStorage.getItem("pickedMonth2Stored")); 
   
-  $.getJSON('https://localhost:44353/car/models?From='+ date1final + '.' +month1final+ '.' + d.getFullYear() + '&To=' + date2final + '.' + month2final + '.' + d.getFullYear(), function(data) {
+  $.getJSON('https://localhost:5001/car/models?From='+ date1final + '.' +month1final+ '.' + d.getFullYear() + '&To=' + date2final + '.' + month2final + '.' + d.getFullYear(), function(data) {
     
     const cars = data["yourCars"];
 
@@ -31,9 +31,9 @@ function getCars(){
 
       console.log(brandOfCar, model, plate);
 
-      $( ".carsDiv" ).append( '<div class="bglogin rounded-xl"><div class="carDiv md:m-auto md:my-1" id="' + (plate) + '"><div class="carDiv1"><div id="carName">' + brandOfCar + ' ' + model + '</div></div>' + 
+      $( ".carsDiv" ).append( '<div id="carBorder" class="bglogin rounded-xl"><div class="carDiv md:m-auto" id="' + (plate) + '"><div class="carDiv1"><div id="carName">' + brandOfCar + ' ' + model + '</div></div>' + 
       '<div class="carDiv2 justify-between"><img id="carPicture" src=' + path + '><button class="orderButton"><div class="orderButtonText font-sans"><span id="price"> ' + price + ' </span> € PER DAY</div><div class="orderButtonText font-sans">ORDER NOW</div></button></div>' + 
-      '<div class="carDiv3"><div class="carIcons">' + fuel + '</div><div class="carIcons">' + numberOfPassenger + '</div><div class="carIcons">' + airConditioner + '</div><div class="carIcons"></div></div></div></div>' );
+      '<div class="carDiv3"><div class="carIcons">Fuel: ' + fuel + '</div><div class="carIcons">Passengers: ' + numberOfPassenger + '</div><div class="carIcons">Air conditioning: ' + airConditioner + '</div><div class="carIcons"></div></div></div></div>' );
 
 
     }
